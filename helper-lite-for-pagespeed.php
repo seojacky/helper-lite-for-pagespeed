18f576ac94f18b6eaef561ff11629bbf13bb3bbb
Eugen <pustojack@list.ru>
2020-07-17 14:12:23 +0300

    Update helper-lite-for-pagespeed.php
    change filter output

--- helper-lite-for-pagespeed.php
+++ helper-lite-for-pagespeed.php
@@ -1,32 +1,32 @@
 <?php
 /**
  * Plugin name: Helper Lite for PageSpeed
- * Description: A faster your site with image attributes decoding="async" & loading="lazy". Help to Up Your Google Page Speed Score. | <a href="http://https://t.me/big_jacky" target="blank_">Author</a> 
- * Version: 2.5
- * Author: @big_jacky 
+ * Description: A faster your site with image attributes decoding="async" & loading="lazy". Help to Up Your Google Page Speed Score. | <a href="http://https://t.me/big_jacky" target="blank_">Author</a>
+ * Version: 2.6
+ * Author: @big_jacky
  * Author URI: https://t.me/big_jacky
  * Plugin URI: https://wordpress.org/plugins/helper-lite-for-pagespeed/
  * GitHub Plugin URI: https://github.com/seojacky/helper-lite-for-pagespeed
-*/
+ */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	return;
-}
+defined('ABSPATH') or exit('No direct script access allowed');
 
-add_filter( 'widget_custom_html_content', 'hlps_add_async_and_lazy', 10 );
-add_filter( 'widget_text', 'hlps_add_async_and_lazy', 10 );
-add_filter( 'the_content','hlps_add_async_and_lazy' );
-add_filter( 'wp_get_attachment_image_attributes', 'hlps_add_async_and_lazy_to_attachment_image', 90 );
+ob_start();
 
-function hlps_add_async_and_lazy( $content ) {
+add_action('shutdown', function ()
+{
+    $content = '';
+
+    // We need to iterate over each ob level, collecting that buffer's output into the final output.
+    while (ob_get_level())
+    {
+        $content .= ob_get_clean();
+    }
+
+    // add async decoding & lazy loading to each image
     $content = str_replace( '<img','<img decoding="async" loading="lazy"', $content );
-    return $content;
-}
 
-function hlps_add_async_and_lazy_to_attachment_image( $attributes ) {
-  $attributes['decoding'] = 'async';
-	if ( ! isset( $attributes['loading'] ) || $attributes['loading'] != 'lazy' ) {
-		$attributes['loading'] = 'lazy';
-	} 
-  return $attributes;
-}
+    // output content
+    echo $content;
+}, 0);
+?>
