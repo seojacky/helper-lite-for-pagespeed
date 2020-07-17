19f45fa46a74b7f73657fe5308858f880115648a
mihdan <mikhail@kobzarev.com>
2020-07-17 11:52:45 +0300

    WPCS

--- helper-lite-for-pagespeed.php
+++ helper-lite-for-pagespeed.php
@@ -1,32 +1,32 @@
 <?php
-/*
+/**
  * Plugin name: Helper Lite for PageSpeed
  * Description: A faster your site with image attributes decoding="async" & loading="lazy". Help to Up Your Google Page Speed Score. | <a href="http://https://t.me/big_jacky" target="blank_">Author</a> 
  * Version: 2.3.7
  * Author: @big_jacky 
  * Author URI: https://t.me/big_jacky
- * Plugin URI: https://github.com/seojacky/helper-lite-for-pagespeed
+ * Plugin URI: https://wordpress.org/plugins/helper-lite-for-pagespeed/
  * GitHub Plugin URI: https://github.com/seojacky/helper-lite-for-pagespeed
 */
-/* Exit if accessed directly */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	return;
 }
 
 add_filter( 'widget_custom_html_content', 'hlps_add_async_and_lazy', 10 );
 add_filter( 'widget_text', 'hlps_add_async_and_lazy', 10 );
-add_filter('the_content','hlps_add_async_and_lazy');
+add_filter( 'the_content','hlps_add_async_and_lazy' );
+add_filter( 'wp_get_attachment_image_attributes', 'hlps_add_async_and_lazy_to_attachment_image', 90 );
+
 function hlps_add_async_and_lazy($content) {
-    $content = str_replace('<img','<img decoding="async" loading="lazy"', $content);
+    $content = str_replace( '<img','<img decoding="async" loading="lazy"', $content );
     return $content;
 }
 
-add_filter( 'wp_get_attachment_image_attributes', 'hlps_add_async_and_lazy_to_attachment_image', 90 );
 function hlps_add_async_and_lazy_to_attachment_image( $attributes ) {
   $attributes['decoding'] = 'async';
-	if(!isset($attributes['loading']) || $attributes['loading'] != 'lazy') {
+	if ( ! isset( $attributes['loading'] ) || $attributes['loading'] != 'lazy' ) {
 		$attributes['loading'] = 'lazy';
 	} 
   return $attributes;
 }
-?>
