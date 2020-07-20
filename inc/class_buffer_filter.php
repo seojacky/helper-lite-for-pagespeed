fdb9f95f55ff4f86ff78820e58610e1e843a64a1
work <bouniafull@gmail.com>
2020-07-20 17:56:26 +0200

    fix buffering conflict

--- inc/class_buffer_filter.php
+++ inc/class_buffer_filter.php
@@ -29,36 +29,7 @@ class HLFP_Buffer_Filter extends HLFP_Filter
         // if not an admin page, start buffering
         if (!is_admin())
         {
-            ob_start();
+            ob_start(array($this, 'filter_content'));
         }
-    }
-
-    /**
-     * Add shutdown hook to filter all buffer's content
-     */
-    public function add_filters()
-    {
-        add_action('shutdown', function ()
-        {
-            // if no buffer, just return
-            if (ob_get_level() == 0)
-            {
-                return;
-            }
-
-            $content = '';
-
-            // We need to iterate over each ob level, collecting that buffer's output into the final output.
-            while (ob_get_level() > 0)
-            {
-                // get buffer content deleting it
-                $content .= ob_get_clean();
-            }
-
-            // filter content
-            $content = $this->filter_content($content);
-
-            echo $content;
-        }, 0);
     }
 }
