89ec8a07c80fd1775825d500f7decb3cffcb37e9
Eugen <pustojack@list.ru>
2020-07-25 17:18:28 +0300

    Update helper-lite-for-pagespeed.php

--- helper-lite-for-pagespeed.php
+++ helper-lite-for-pagespeed.php
@@ -3,7 +3,7 @@
  * Plugin name: Helper Lite for PageSpeed
  * Description: A faster your site with image attributes decoding="async" & loading="lazy". Remove problem "Does not use passive listeners to improve scrolling performance". Help to Up Your Google PageSpeed Insights Score. | <a href="https://t.me/wp_booster" target="blank_">telegram WP Boost</a> | <a href="http://https://github.com/seojacky/helper-lite-for-pagespeed" target="blank_">plugin on GitHub</a>
  * Version: 2.5.4
- * Author: seojacky, Mikhail Kobzarev, Каренина
+ * Author: seojacky, Каренина
  * Author URI: https://t.me/big_jacky
  * Plugin URI: https://wordpress.org/plugins/helper-lite-for-pagespeed/
  * GitHub Plugin URI: https://github.com/seojacky/helper-lite-for-pagespeed
