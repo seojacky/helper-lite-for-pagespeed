68679d418f5249f703263dfa28990fa25c8515fc
mihdan <mikhail@kobzarev.com>
2021-02-12 19:01:12 +0300

    Добавил возможность передавать callable функцию в описание поля

--- src/admin/class-admin-fields.php
+++ src/admin/class-admin-fields.php
@@ -407,7 +407,12 @@ class HLFP_OSA
     {
         if (!empty($args['desc']))
         {
-            $desc = sprintf('<p class="description">%s</p>', $args['desc']);
+	        $desc = sprintf(
+		        '<p class="description">%s</p>',
+		        is_callable( $args['desc'] )
+			        ? call_user_func( $args['desc'] )
+			        : $args['desc']
+	        );
         }
         else
         {
