6d47bddc2a4f8d57a3358b06bece049b9cb1fb6b
Eugen <kalinsky30@gmail.com>
2024-11-15 14:01:13 +0200

    Update helper-lite-for-pagespeed.php

--- helper-lite-for-pagespeed.php
+++ helper-lite-for-pagespeed.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Helper Lite for PageSpeed
  * Description: Speed up your site with attributes decoding="async" & loading="lazy" for &lt;img&gt; and &lt;iframe&gt;. Removes problem "Does not use passive listeners to improve scrolling performance". Help to Up Your Google PageSpeed Insights Score.
- * Version: 3.1.6.2
+ * Version: 3.1.7
  * Author: WP Booster
  * Author URI: https://wp-booster.com/
  * Plugin URI: https://wordpress.org/plugins/helper-lite-for-pagespeed/
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 define( 'HLFP_FILE', __FILE__ );
 define( 'HLFP_DIR', __DIR__ );
 define( 'HLFP_URL', plugin_dir_url( __FILE__ ) );
-define( 'HLFP_VERSION', '3.1.6.2' );
+define( 'HLFP_VERSION', '3.1.7' );
 define( 'HLFP_TITLE', __( 'PageSpeed Helper', 'helper-lite-for-pagespeed' ) );
 define( 'HLFP_SLUG', 'helper-lite-for-pagespeed' );
 define( 'HLFP_URL_JS', HLFP_URL . 'js' );
